e4ab69f4e0741d857e9755efba561606b3b8ae19
pzlll <1315255648@qq.com>
2021-04-22 22:11:15 +0800

    add method maxSumSubmatrix.

--- src/binarysearch/Solution.java
+++ src/binarysearch/Solution.java
@@ -1,5 +1,7 @@
 package binarysearch;
 
+import java.util.TreeSet;
+
 public class Solution {
     /**
      * 解题思路：通过元素与下一元素的大小关系可确定峰值元素位置
@@ -121,5 +123,42 @@ public class Solution {
         return nums[left];
     }
 
+    /**
+     * 解题思路：1.对行设置上下界限
+     *          2.用数组存储每列的前缀和
+     *          3.把区间Sr - Sl<=k的最大值转化为S1 >=Sr - k的最小值
+     *          4.用二叉树的ceiling作为第三步查找的工具
+     * @param matrix
+     * @param k
+     * @return
+     */
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < m; i++) {
+            int[] sum = new int[n];
+            for (int j = i; j < m; j++) {
+                for (int l = 0; l < n; l++) {
+                    sum[l] += matrix[j][l];
+                }
+                TreeSet<Integer> set = new TreeSet<>();
+                int s = 0;
+                set.add(0);
+                for (int v :
+                        sum) {
+                    s += v;
+                    Integer ceiling = set.ceiling(s - k);
+                    if(ceiling != null) {
+                        max = Math.max(max, s - ceiling);
+                    }
+                    set.add(s);
+                }
+            }
+        }
+
+        return max;
+    }
 
 }
