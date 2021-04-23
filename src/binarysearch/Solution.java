0b13d17ee54f80620db3340c01a58d2387f218c0
kieren <1315255648@qq.com>
2021-04-23 14:17:45 +0800

    add method removeDuplicates.

--- src/binarysearch/Solution.java
+++ src/binarysearch/Solution.java
@@ -150,5 +150,17 @@ public class Solution {
         return false;
     }
 
-
+    public int removeDuplicates(int[] nums) {
+        int i = 1;
+        int j = 1;
+        while(j < nums.length) {
+            if(nums[j] != nums[j -
+                    1]) {
+                nums[i] = nums[j];
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
 }
