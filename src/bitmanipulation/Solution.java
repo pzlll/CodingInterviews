6b2500251ce47796722e4b9e569b2f606ef91b10
kieren <1315255648@qq.com>
2021-05-07 14:56:30 +0800

    add method xorOperation.

--- src/bitmanipulation/Solution.java
+++ src/bitmanipulation/Solution.java
@@ -32,6 +32,7 @@ public class Solution {
      * 解题思路：利用异或性质
      * 任意整数和自身做异或运算的结果都等于 00，即 x ^ x = 0；
      * 任意整数和 00 做异或运算的结果都等于其自身，即 x ^ 0 = 0 ^ x = x。
+     *
      * @param encoded
      * @param first
      * @return
@@ -45,5 +46,51 @@ public class Solution {
 
         return ans;
 
+    }
+
+
+    /**
+     * 解题思路：利用异或性质
+     * 1. x⊕x=0；
+     * 2. 4i⊕(4i+1)⊕(4i+2)⊕(4i+3)=0
+     * 方法：1.公式为start⊕(start+2i)⊕(start+4i)⊕⋯⊕(start+2(n−1))
+     *          处理最后一位（若start和n为奇数，则为1,否则为0）
+     *      2.将公式转化为(s⊕(s+1)⊕(s+2)⊕⋯⊕(s+n−1))×2+e
+     *      3.函数sumXor(x): 0⊕1⊕2⊕⋯⊕x
+     *                          x           x=4k
+     *                          1           x=4k+1
+     *                          x+1         x=4k+2
+     *                          0           x=4k+3
+     *      4.公式转化为(sumXor(s−1)⊕sumXor(s+n−1))*2+e
+     * @param n
+     * @param start
+     * @return
+     */
+    public int xorOperation(int n, int start) {
+//        int res = 0;
+//        for (int i = 0; i < n; i++) {
+//            res ^= (start + 2 * i);
+//        }
+//
+//        return res;
+        int res = 0;
+        int s = start / 2;
+        int e = (start & 1) & (n & 1);
+        res = (sumXor(s - 1) ^ sumXor(s + n - 1)) * 2 + e;
+        return res;
+    }
+
+    private int sumXor(int k) {
+        switch (k % 4) {
+            case 0:
+                return k;
+            case 1:
+                return 1;
+            case 2:
+                return (k + 1);
+            case 3:
+                return 0;
+        }
+        return 0;
     }
 }
