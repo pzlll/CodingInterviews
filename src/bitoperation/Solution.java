78f0f67776dd1a6be504798c2d8405666ed989e6
kieren <1315255648@qq.com>
2021-05-19 15:46:55 +0800

    add method kthLargestValue.

--- src/bitoperation/Solution.java
+++ src/bitoperation/Solution.java
@@ -1,9 +1,6 @@
 package bitoperation;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
     public int NumberOf1(int n) {
@@ -20,13 +17,14 @@ public class Solution {
         return num;
     }
 
-    public static void main(String[] args) {
-        int n = 16;
-        int i = new Solution().NumberOf1(-1);
-        System.out.println(i);
 
-    }
-
+    /**
+     * 解题思路：使用前缀和存储对应下标的前缀和
+     * 将题目转化为与下标k前缀和相同以及下标值在它之前的所有下标
+     * 使用两个哈系表减少查找时间复杂度
+     * @param arr
+     * @return
+     */
     public int countTriplets(int[] arr) {
         int Xor = 0;
         int count = 0;
@@ -50,5 +48,37 @@ public class Solution {
 
         return count;
 
+    }
+
+    /**
+     * 解题思路：使用前缀和存储对应下标的前缀异或和
+     * 遍历数组，找出第K大的前缀和（使用快速排序或堆排序）
+     * @param matrix
+     * @param k
+     * @return
+     */
+    public int kthLargestValue(int[][] matrix, int k) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] Xor = new int[m+1][n+1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                Xor[i][j] = Xor[i-1][j] ^ Xor[i][j-1] ^ Xor[i-1][j-1] ^ matrix[i-1][j-1];
+            }
+        }
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int val = Xor[i+1][j+1];
+                priorityQueue.offer(val);
+                if(priorityQueue.size() > k) {
+                    priorityQueue.poll();
+                }
+            }
+        }
+
+        return priorityQueue.peek();
     }
 }
