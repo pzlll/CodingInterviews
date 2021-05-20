dab955f026f119bf6b029ead56411dddf57c9190
kieren <1315255648@qq.com>
2021-05-20 14:49:43 +0800

    add method topKFrequent.

--- src/heap/Solution.java
+++ src/heap/Solution.java
@@ -1,7 +1,7 @@
 package heap;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
@@ -73,6 +73,48 @@ public class Solution {
             compounds[point] = temp;
         }
 
+    }
+
+    /**
+     * 解题思路：使用哈系表存储对应单词的频率
+     * 使用优先队列的小顶堆找出前K个单词
+     * 优先队列的使用：默认是小顶堆，以o1为插入值，返回结果为正值则表示o1比o2大，返回结果为负值则表示o1比o2小
+     * @param words
+     * @param k
+     * @return
+     */
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> freq = new HashMap<>();
+        int n = words.length;
+        for (int i = 0; i < n; i++) {
+            freq.put(words[i], freq.getOrDefault(words[i], 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<String, Integer>> kQueue = new PriorityQueue<>(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                int v1 = o1.getValue() - o2.getValue();
+                if(v1 != 0) {
+                    return v1;
+                }
+                return o2.getKey().compareTo(o1.getKey());
+            }
+        });
+
+        for (Map.Entry<String, Integer> entry :
+             freq.entrySet()) {
+            kQueue.offer(entry);
+            if(kQueue.size() > k) {
+                kQueue.poll();
+            }
+        }
+
+        String[] ret = new String[k];
+        for (int i = (k-1); i >= 0; i--) {
+            ret[i] = kQueue.poll().getKey();
+        }
+
+        return Arrays.asList(ret);
     }
 }
 class Compound {
