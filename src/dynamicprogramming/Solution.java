e2e96683b689b9a97a087f41e67f46a22dc585c9
kieren <1315255648@qq.com>
2021-06-04 14:09:46 +0800

    add method findMaxLength.

--- src/dynamicprogramming/Solution.java
+++ src/dynamicprogramming/Solution.java
@@ -735,4 +735,24 @@ public class Solution {
 
         return false;
     }
+
+    public int findMaxLength(int[] nums) {
+
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int length = 0;
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += (nums[i] ==0 ? -1 : 1);
+            if(map.containsKey(sum)) {
+                length = Math.max(length, (i - map.get(sum)));
+            }else {
+                map.put(sum, i);
+            }
+        }
+
+        return length;
+    }
+
+
 }
