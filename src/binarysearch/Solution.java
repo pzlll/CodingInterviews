ca7907a3b06f6dd9a7278bf81752861083549b28
kieren <1315255648@qq.com>
2021-06-13 10:17:17 +0800

    add method firstBadVersion.

--- src/binarysearch/Solution.java
+++ src/binarysearch/Solution.java
@@ -324,4 +324,30 @@ public class Solution {
             return false;
         }
     }
+
+    /**
+     * 解题思路：二分查找
+     * @param n
+     * @return
+     */
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while(left < right) {
+//            计算溢出
+//            int mid = (right + left) / 2;
+            int mid = left + (right - left) / 2;
+            if(isBadVersion(mid)) {
+                right = mid;
+            }else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private boolean isBadVersion(int mid) {
+        return false;
+    }
 }
