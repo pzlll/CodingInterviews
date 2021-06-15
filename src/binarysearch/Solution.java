495782ce37d2241d85fcc9ec2d50feed81f114d4
kieren <1315255648@qq.com>
2021-06-15 20:55:10 +0800

    add method peakIndexInMountainArray.

--- src/binarysearch/Solution.java
+++ src/binarysearch/Solution.java
@@ -372,4 +372,21 @@ public class Solution {
     private int guess(int num) {
         return 0;
     }
+
+
+    public int peakIndexInMountainArray(int[] arr) {
+        int n = arr.length;
+        int left = 0;
+        int right = n-1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if(arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            }else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
 }
