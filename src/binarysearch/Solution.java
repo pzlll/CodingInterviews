6c4085c92842154891e4ec939ee97907dbaf214d
kieren <1315255648@qq.com>
2021-06-19 19:29:39 +0800

    add method smallestGoodBase.

--- src/binarysearch/Solution.java
+++ src/binarysearch/Solution.java
@@ -1,6 +1,6 @@
 package binarysearch;
 
-import java.util.TreeSet;
+import java.util.*;
 
 public class Solution {
     /**
@@ -388,5 +388,26 @@ public class Solution {
         }
 
         return left;
+    }
+
+    public String smallestGoodBase(String n) {
+        long nVal = Long.parseLong(n);
+        int mMax = (int)Math.floor(Math.log(nVal) / Math.log(2));
+        for(int m = mMax; m > 1; m--) {
+            int k = (int) Math.pow(nVal, 1.0/m);
+            long mul = 1, sum = 1;
+            for (int i = 0; i < m; i++) {
+                mul *= k;
+                sum += mul;
+            }
+
+            if(nVal == sum) {
+                return Integer.toString(k);
+            }
+        }
+
+
+
+        return Long.toString(nVal-1);
     }
 }
