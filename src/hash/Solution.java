d1691d3b966e49373a5c118d32ca4704da8746a8
kieren <1315255648@qq.com>
2021-06-24 15:12:48 +0800

    add method maxPoints.

--- src/hash/Solution.java
+++ src/hash/Solution.java
@@ -25,6 +25,7 @@ public class Solution {
 
     /**
      * 解题关键：任何一个int型的数，它的next都是三位数以内，所以它不会无限循环，用哈希表存储其足迹
+     *
      * @param n
      * @return
      */
@@ -41,7 +42,7 @@ public class Solution {
             n = sum;
 
         }
-        if(sum != 1) {
+        if (sum != 1) {
             return false;
         }
 
@@ -55,8 +56,8 @@ public class Solution {
         }
 
         int ret = 0;
-        for (Integer in:
-             set) {
+        for (Integer in :
+                set) {
             ret += in + 1;
         }
 
@@ -67,6 +68,7 @@ public class Solution {
      * 解题思路：求砖块的最小值转化为求边缘的最大值
      * 遍历每一行，通过每一行的元素，将其对应的边缘数量加一
      * 使用哈系表存储对应的边缘的数量
+     *
      * @param wall
      * @return
      */
@@ -82,7 +84,7 @@ public class Solution {
                 index += list.get(i);
                 map.put(index, map.getOrDefault(index, 0) + 1);
             }
-            
+
         }
 
         int max = 0;
@@ -99,6 +101,7 @@ public class Solution {
      * 1. 判断两字符串长度是否相等
      * 2. 用长度为26的数组（哈希表）统计字符串s中字符的个数
      * 3. 遍历字符串t，每遇到一个字符则减去数组对应字符的个数，若个数小于0，则两字符串不匹配
+     *
      * @param s
      * @param t
      * @return
@@ -120,7 +123,75 @@ public class Solution {
         return true;
     }
 
+    /**
+     * 解题思路：
+     * 对于每个点，计算出经过该点的直线包含的顶点最大数量
+     * 对于固定一个点i，若其余两个点j、k与其在同一顶点上，则他们的i与j的斜率等于i与k的斜率
+     * 对于固定一个点i，使用哈希表存储不同斜率及其数量，每遍历一个顶点，更新一次最大值
+     * 使用二元组（mx,my）存储斜率
+     * 保证斜率的分母为正，使用公约数简化分子分母
+     * 因为分子和分母位数之和小于int范围，使用第n位存储分子，高n位存储分母
+     *
+     * @param points
+     * @return
+     */
+    public int maxPoints(int[][] points) {
+        int n = points.length;
 
+        if (n <= 2) {
+            return n;
+        }
+
+        int ret = 0;
+        for (int i = 0; i < n; i++) {
+            if (ret >= (n - i) || ret > n / 2) {
+                return ret;
+            }
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int j = i + 1; j < n; j++) {
+                int x = points[i][0] - points[j][0];
+                int y = points[i][1] - points[j][1];
+                if (x == 0) {
+                    y = 1;
+                } else if (y == 0) {
+                    x = 1;
+                }
+                if (y < 0) {
+                    x = -x;
+                    y = -y;
+                }
+
+                int mx = x / gcd(Math.abs(x), Math.abs(y));
+                int my = y / gcd(Math.abs(x), Math.abs(y));
+
+                int index = mx + 10000 + 2 * 10000 * my;
+                map.put(index, map.getOrDefault(index, 0) + 1);
+            }
+
+            int maxn = 0;
+            for (Map.Entry<Integer, Integer> entry :
+                    map.entrySet()) {
+                int value = entry.getValue();
+                maxn = Math.max(maxn, value + 1);
+            }
+
+            ret = Math.max(ret, maxn);
+        }
+
+        return ret;
+    }
+
+    private int gcd(int x, int y) {
+        while (x * y != 0) {
+            if (x > y) {
+                x %= y;
+            } else {
+                y %= x;
+            }
+        }
+
+        return x > y ? x : y;
+    }
 
 
 }
