421978d133f0fdefef9c399f15290e081f105080
kieren <1315255648@qq.com>
2021-07-20 15:36:39 +0800

    add method minPairSum.

--- src/doublepointer/Solution.java
+++ src/doublepointer/Solution.java
@@ -2,6 +2,7 @@ package doublepointer;
 
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 class ListNode {
@@ -562,6 +563,19 @@ public class Solution {
         }
 
         return ret;
+    }
+
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        int max = 0;
+        for (int i = 0; i < n/2; i++) {
+            max = Math.max(max, nums[i] + nums[n-1-i]);
+        }
+
+        return max;
     }
 
 }
