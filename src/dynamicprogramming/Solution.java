01f32e2189ac0d257d84e634b42790bbb7417764
kieren <1315255648@qq.com>
2021-08-08 09:22:27 +0800

    add method tribonacci.

--- src/dynamicprogramming/Solution.java
+++ src/dynamicprogramming/Solution.java
@@ -1184,4 +1184,30 @@ public class Solution {
         return true;
     }
 
+    public int tribonacci(int n) {
+        if(n == 0 || n == 1){
+            return n;
+        }
+
+        if(n == 2) {
+            return 1;
+        }
+
+
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int d = 0;
+
+        while (n > 2) {
+            d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+            n--;
+        }
+
+        return d;
+    }
+
 }
