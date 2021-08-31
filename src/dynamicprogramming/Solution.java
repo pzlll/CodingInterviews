07307c226d1486c94758f1f32989a9d014e8462a
kieren <1315255648@qq.com>
2021-08-31 20:32:33 +0800

    add method corpFlightBookings.

--- src/dynamicprogramming/Solution.java
+++ src/dynamicprogramming/Solution.java
@@ -1345,4 +1345,33 @@ public class Solution {
         return sum;
     }
 
+    /**
+     * 解题思路：差分数组
+     * 将各航班增加的人数填入差分数组，最后通过前缀和求得每个航班的人数
+     * @param bookings
+     * @param n
+     * @return
+     */
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] dp = new int[n];
+        for (int i = 0; i < bookings.length; i++) {
+            int l = bookings[i][0];
+            int r = bookings[i][1];
+            int num = bookings[i][2];
+
+            dp[l-1] += num;
+            if(r != n) {
+                dp[r] -= num;
+            }
+        }
+
+        int[] res = new int[n];
+        res[0] = dp[0];
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i-1] + dp[i];
+        }
+
+        return res;
+    }
+
 }
