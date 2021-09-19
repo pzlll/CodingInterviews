b72d1e165b714f19f63cfd47eebe54376548e0ab
pzlll <1315255648@qq.com>
2021-09-19 22:04:28 +0800

    add method pickIndex.

--- src/dynamicprogramming/Solution.java
+++ src/dynamicprogramming/Solution.java
@@ -1401,4 +1401,22 @@ public class Solution {
 
     }
 
+        public int minSteps(int n) {
+            int[] dp = new int[n+1];
+
+            Arrays.fill(dp, Integer.MAX_VALUE);
+
+            dp[1] = 0;
+            for (int i = 2; i <= n; i++) {
+                for (int j = 1; (double)j <= Math.sqrt((double)i); j++) {
+                    if(i % j == 0) {
+                        dp[i] = Math.min(dp[i], dp[j] + i / j);
+                        dp[i] = Math.min(dp[i], dp[i/j] + j);
+                    }
+                }
+            }
+
+            return dp[n];
+        }
+
 }
