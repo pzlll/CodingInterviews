e182aab0f613fc96acab8ea0cc883522ef8a03dd
pzlll <1315255648@qq.com>
2022-02-23 10:25:11 +0800

    add method reverseOnlyLetters.

--- src/doublepointer/Solution.java
+++ src/doublepointer/Solution.java
@@ -763,4 +763,25 @@ public class Solution {
         return j == m;
     }
 
+    public String reverseOnlyLetters(String s) {
+        char[] chars = s.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        while (start < end) {
+            while (start < end && !Character.isAlphabetic(chars[start])) {
+                start++;
+            }
+            while (start < end && !Character.isAlphabetic(chars[end])) {
+                end--;
+            }
+
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+        }
+
+        return new String(chars);
+
+    }
+
 }
--- src/tree/Codec.java
+++ src/tree/Codec.java
@@ -67,4 +67,6 @@ public class Codec {
 
         return root;
     }
+
+
 }
