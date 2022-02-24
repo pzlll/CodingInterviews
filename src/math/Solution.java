df548426ac76549da1ddc4a7573229f025b128da
pzlll <1315255648@qq.com>
2022-02-24 17:27:30 +0800

    add method trailingZeroes.

--- src/math/Solution.java
+++ src/math/Solution.java
@@ -513,4 +513,16 @@ public class Solution {
         return ret;
     }
 
+    public int trailingZeroes(int n) {
+
+        int count = 0;
+        int divider = 5;
+        while(divider <= n) {
+            count += (n / divider);
+            divider *= 5;
+        }
+
+        return count;
+    }
+
 }
