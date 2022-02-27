afe022e3223764e457b21ae3c97a673340b3ccbe
pzlll <1315255648@qq.com>
2022-02-27 15:04:05 +0800

    add method mySqrt.

--- src/math/Solution.java
+++ src/math/Solution.java
@@ -553,6 +553,27 @@ public class Solution {
         return flag ? 1.0/res : res;
     }
 
+    public int mySqrt(int x) {
+        long X = x;
+        if(x == 0 || x == 1){
+            return x;
+        }
+        int start = 1;
+        int end = x;
+        while(start < end) {
+            int mid = (end - start) / 2 + start;
+            if(mid * mid == X) {
+                return mid;
+            }else if(mid * mid > X) {
+                end = mid;
+            }else {
+                start = mid + 1;
+            }
+        }
+
+        return start == 1 ? 1 : start - 1;
+    }
+
 
 
 }
