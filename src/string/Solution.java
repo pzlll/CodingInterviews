dbd3894946bdbbd7846c98be1ca8f17abb4e71ee
pzlll <1315255648@qq.com>
2022-03-05 11:24:00 +0800

    add method findLUSlength.

--- src/string/Solution.java
+++ src/string/Solution.java
@@ -534,4 +534,13 @@ public class Solution {
         }
         return res;
     }
+
+    public int findLUSlength(String a, String b) {
+        if(a.length() != b.length()) {
+            return a.length() > b.length() ? a.length() : b.length();
+        }else {
+            return a.equals(b) ? -1 : a.length();
+        }
+
+    }
 }
