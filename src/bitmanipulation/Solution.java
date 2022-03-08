ef24ba3460ac89c2c6ca5753f9125f44248f5cb4
pzlll <1315255648@qq.com>
2022-03-08 17:04:01 +0800

    add method getSum.

--- src/bitmanipulation/Solution.java
+++ src/bitmanipulation/Solution.java
@@ -390,4 +390,22 @@ public class Solution {
     }
 
 
+    public int getSum(int a, int b) {
+        int sum = 0;
+        int carry = 0;
+        for (int i = 0; i < 32; i++) {
+            int a1 = a & 1;
+            int b1 = b & 1;
+
+            sum = sum | ((a1 ^ b1 ^ carry) << i);
+
+            carry = (a1 & b1) | ((a1 ^ b1) & carry);
+            a >>= 1;
+            b >>= 1;
+        }
+
+        return sum;
+    }
+
+
 }
