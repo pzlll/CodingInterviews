52ef886f78b5dd071beeed43b1c00f72476427a5
pzlll <1315255648@qq.com>
2022-07-27 15:10:28 +0800

    add method firstMissingPositive.

--- src/hash/Solution.java
+++ src/hash/Solution.java
@@ -449,5 +449,32 @@ public class Solution {
     }
 
 
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(Math.abs(nums[i]) >= 1 && Math.abs(nums[i]) <= n) {
+                if(nums[Math.abs(nums[i]) - 1] > 0) {
+                    nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i]) - 1];
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(nums[i] > 0) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+
+
+    }
+
 
 }
