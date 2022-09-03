0605f14bb98c08ef3f36f4d614ea3ff7095f3ee2
pzlll <1315255648@qq.com>
2022-09-03 11:43:17 +0800

    add method divideLists.

--- src/divideandconquer/Solution.java
+++ src/divideandconquer/Solution.java
@@ -1,6 +1,19 @@
 package divideandconquer;
 
+import java.util.List;
+
 public class Solution {
+
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
     /**
      * 解题思路：（分治法）统计各字符的频数，遍历字符串，若某一字符的频数小于k，则递归求其左右子串最大值
      * 若所有字符频数都大于等于k，则返回字符串长度
@@ -25,5 +38,53 @@ public class Solution {
         }
 
         return end - start +1;
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0) {
+            return null;
+        }
+
+
+        ListNode res = divideLists(lists, 0, lists.length -1);
+
+        return res;
+    }
+
+    private ListNode divideLists(ListNode[] lists, int start, int end) {
+        if(start == end) {
+            return lists[start];
+        }
+
+        int mid = (start + end) / 2;
+
+        ListNode left = divideLists(lists, start, mid);
+        ListNode right = divideLists(lists, mid + 1, end);
+
+        ListNode p = new ListNode();
+        ListNode root =  p;
+
+        while (left != null && right != null) {
+            if(left.val < right.val) {
+                p.next = left;
+                p = p.next;
+                left = left.next;
+            }else {
+                p.next = right;
+                p = p.next;
+                right = right.next;
+            }
+        }
+
+        if(left != null) {
+            p.next = left;
+        }
+
+        if(right != null) {
+            p.next = right;
+        }
+
+        return root.next;
+
     }
 }
--- src/dynamicprogramming/Solution.java
+++ src/dynamicprogramming/Solution.java
@@ -1648,5 +1648,36 @@ public class Solution {
 
     }
 
+    public int findLongestChain(int[][] pairs) {
+        int length = pairs.length;
+
+        int[] dp = new int[length];
+
+        Arrays.sort(pairs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                int o = o1[0] - o2[0];
+                return o == 0 ? o1[1] - o2[1] : o;
+            }
+        });
+
+        for (int i = 0; i < length; i++) {
+            int count = 0;
+            for (int j = 0; j < i; j++) {
+                if(pairs[j][1] < pairs[i][0]) {
+                    count = Math.max(count, dp[j]);
+                }
+            }
+            dp[i] = count + 1;
+        }
+
+        int max = 0;
+        for (int i = 0; i < length; i++) {
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+
 
 }
